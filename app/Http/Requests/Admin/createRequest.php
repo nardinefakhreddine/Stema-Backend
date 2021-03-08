1ed5094d32e5b310ec34cc2b1409cc29bd74b859
Nardine <nardinefakhreddine@gmail.com>
2021-03-08 13:10:36 +0200

    requests update

--- app/Http/Requests/Admin/createRequest.php
+++ app/Http/Requests/Admin/createRequest.php
@@ -25,7 +25,7 @@ class createRequest extends FormRequest
     {
         return [
             'name'=>'required|String|min:3',
-            "username"=>'required|String',
+            "username"=>'required|String|min:3',
             "email"=>'required|email',
             "password"=>'required|String|min:6'
         ];
