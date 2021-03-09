08899e1f7270a468b0cfa5c29cdb69d5910b6e30
Nardine <nardinefakhreddine@gmail.com>
2021-03-09 19:35:54 +0200

    favorite

---
+++ app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Favorite;
+class FavoriteController extends Controller
+{
+    public function get($id){
+        $data=Favorite::where('user_id',$id)->get();
+        if ($data) {
+            return response()->json($data);
+        }
+        return response()->json([
+            "message" => "Couldn't get Data list"
+        ], 400);
+    }
+
+ public function create($productID,$userID){
+    $data=Favorite::create([
+        'user_id'=>$request->user_id,
+        'product_id'=>$request->product_id,
+        
+     
+ ]);
+ return response()->json($data);
+      
+ }
+
+ public function delete($id){
+    $data=Favorite::find($id)->delete();
+    if(!$data){
+      return response()->json(["message"=>"Couldn't Delete Data "],400);
+  }else{
+      return response()->json(["message"=>"Successfully Delete Data "],200);
+  }
+}
+}
